df72f6336791c0226b8a2eeb9da3a8a6d1c1b95d
lantianwei <xiaolanlub@outlook.com>
2022-10-06 23:05:55 +1100

    feat: title

--- components/layout.js
+++ components/layout.js
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import DynamicNavBar from "./dynamicNavBar";
 
 const Layout = ({ children }) => {
   return (
     <div className=" flex flex-col h-screen justify-between">
+      <Head>
+        <title>Vaping is not cool</title>
+      </Head>
       {children}
       <div className="fixed z-30 bottom-0 w-full">
         <DynamicNavBar />
